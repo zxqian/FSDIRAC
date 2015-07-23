9a40b5d22b7beb51769618dd35bcedd361e5df47
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2015-07-23 15:27:44 +0200

    first import

---
+++ DataManagementSystem/scripts/dirac-mount.py
@@ -0,0 +1,12 @@
+############################################################
+#
+# dirac-mount command to mount DIRAC FS
+#
+############################################################
+
+from DIRAC.Core.Base import Script
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s <pilot reference>' % Script.scriptName ] ) )
+
+Script.parseCommandLine( ignoreErrors = True )
