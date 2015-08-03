4841e5cb2f1de01a93fe75ef9da9f9ffbbeae5b8
Zuxuan Qian <zqian@cppm.in2p3.fr>
2015-08-03 15:20:36 +0200

    new version

--- DataManagementSystem/scripts/dirac-mount.py
+++ DataManagementSystem/scripts/dirac-mount.py
@@ -4,7 +4,16 @@
 dirac-mount command to mount DIRAC FS
 """
 
+import os
+
 from DIRAC.Core.Base import Script
+from DIRAC import S_OK
+
+defaultSE = ""
+def setDefaultSE( value ):
+    global defaultSE
+    defaultSE = value
+    return S_OK()
 
 Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      'Usage:',
@@ -18,42 +27,42 @@ Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      ] )
                         )
 
-Script.registerSwitch( "se", "se=", "Set SE disk" )
+Script.registerSwitch( "S:", "se=", "Default Storage Element", setDefaultSE )
 Script.parseCommandLine( ignoreErrors = True )
 
-import os
+from DIRAC import exit as DIRACexit
+
 args = Script.getPositionalArgs()
 if len( args ):
-  arg = args[ 0 ]
-  aarg = os.path.abspath( arg )
-  if os.path.isdir(aarg):
-    if os.listdir(aarg):
-      print  "["+arg+"]"+" is not empty"
-      Script.showHelp()
-      DIRAC.exit( 0 )
-  else:
-    print "["+arg+"]"+" is not a valid directory"
+    arg = args[ 0 ]
+    aarg = os.path.abspath( arg )
+    if os.path.isdir(aarg):
+        if os.listdir(aarg):
+            print  "["+arg+"]"+" is not empty"
+            Script.showHelp()
+            DIRAC.exit( 0 )
+    else:
+        print "["+arg+"]"+" is not a valid directory"
+        Script.showHelp()
+        DIRAC.exit( 0 )
+else:
+    print "no mount point"
     Script.showHelp()
     DIRAC.exit( 0 )
-else:
-  print "no mount point"
-  Script.showHelp()
-  DIRAC.exit( 0 )
 
 # call DiracFS.py
 
 import sys
-newarg = []
-for a in sys.argv:
-  if not a.find("--se"):
-    ss = a.split("=")
-    newarg.append("-o SE="+ss[1])
-  else:
-    newarg.append(a)
+import DiracFS
+if defaultSE:
+    for i, item in enumerate(sys.argv):
+        if not item.find("--se="):
+            sys.argv.pop(i)
+            sys.argv.append("-o") 
+            sys.argv.append("SE=" + defaultSE) 
+            break
+        elif not item.find("--se") or not item.find("-S"):
+            sys.argv[i] = "-o"
+            sys.argv[i+1] = "SE=" + defaultSE
+DiracFS.main(sys.argv)
 
-newcmd = "DiracFS.py"
-for index, item in enumerate(newarg):
-  if not index==0:
-    newcmd += " "
-    newcmd += item
-os.system(newcmd)
